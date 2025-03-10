5df59d0a6ab26dd5e474e68345e3f29b22697c85
mckfq <m.freirequeiros@gmail.com>
2025-03-10 17:29:19 +0000

    Create automat.sh

---
+++ automat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd geral
+
+# TVI - update the stream URL of TVI
+
+sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
+
+# CNN Portugal - update the stream URL of CNN Portugal
+
+sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
