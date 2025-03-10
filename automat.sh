44bf0fd17ba3ec9f89a1a1983e226f746061ee34
mckfq <m.freirequeiros@gmail.com>
2025-03-10 22:48:31 +0000

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -2,10 +2,8 @@
 
 cd geral
 
-# TVI - update the stream URL of TVI
-
+# canal1
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
 
-# CNN Portugal - update the stream URL of CNN Portugal
-
+# canal2
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
