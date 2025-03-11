40fa9d2d5e14b39f24bff2ff198ae3dd59d717b6
mckfq <m.freirequeiros@gmail.com>
2025-03-11 19:22:25 +0000

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -8,14 +8,30 @@ sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/play
 # PT CNN
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
 
-# FR M6
-sed -i "/m6france.m3u8 c https://sv1.data-stream.top/$(wget hhttps://sv1.data-stream.top/ -o /dev/null -O -)/hls/m6france.m3u8" geral.m3u
+# FR M6 Récupérer le contenu de la page
+PAGE_CONTENT=$(curl -s "https://www.stream4free.tv/m6-live-streaming")
+# Extraire l'URL dynamique de la page
+DYNAMIC_URL=$(echo "$PAGE_CONTENT" | grep -oP 'https://sv1.data-stream.top/[^"]+/hls/m6france.m3u8')
+# Remplacer uniquement la ligne qui contient "w9france" dans le fichier M3U
+sed -i "/m6france/c $DYNAMIC_URL" geral.m3u
 
-# FR W9
-sed -i "/w9france.m3u8 c https://sv7.data-stream.top/$(wget hhttps://sv7.data-stream.top/ -o /dev/null -O -)/hls/w9france.m3u8" geral.m3u
+# FR W9 Récupérer le contenu de la page
+PAGE_CONTENT=$(curl -s "https://www.stream4free.tv/mw9-france)
+# Extraire l'URL dynamique de la page
+DYNAMIC_URL=$(echo "$PAGE_CONTENT" | grep -oP 'https://sv7.data-stream.top/[^"]+/hls/w9france.m3u8')
+# Remplacer uniquement la ligne qui contient "w9france" dans le fichier M3U
+sed -i "/w6france/c $DYNAMIC_URL" geral.m3u
 
-#FR 6ter
-sed -i "/6ter.m3u8 c https://sv1.data-stream.top/$(wget hhttps://sv1.data-stream.top/ -o /dev/null -O -)/hls/6ter.m3u8" geral.m3u
+# FR 6Ter Récupérer le contenu de la page
+PAGE_CONTENT=$(curl -s "https://www.stream4free.tv/6ter-france")
+# Extraire l'URL dynamique de la page
+DYNAMIC_URL=$(echo "$PAGE_CONTENT" | grep -oP 'https://sv1.data-stream.top/[^"]+/hls/6ter.m3u8')
+# Remplacer uniquement la ligne qui contient "w9france" dans le fichier M3U
+sed -i "/6ter.m3u/c $DYNAMIC_URL" geral.m3u
 
-#FR TF1 Series Films
-sed -i "/seriefilmes.m3u8 c https://sv1.data-stream.top/$(wget hhttps://sv1.data-stream.top/ -o /dev/null -O -)/hls/seriefilmes" geral.m3u
+# FR TF1SF Récupérer le contenu de la page
+PAGE_CONTENT=$(curl -s "https://www.stream4free.tv/tf1-series-films")
+# Extraire l'URL dynamique de la page
+DYNAMIC_URL=$(echo "$PAGE_CONTENT" | grep -oP 'https://sv1.data-stream.top/[^"]+/hls/seriefilmes.m3u8')
+# Remplacer uniquement la ligne qui contient "w9france" dans le fichier M3U
+sed -i "/seriefilmes/c $DYNAMIC_URL" geral.m3u
