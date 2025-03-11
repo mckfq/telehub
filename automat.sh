1a739dddc6fd72c254247708f9fd8cf011096ed7
mckfq <m.freirequeiros@gmail.com>
2025-03-11 17:29:44 +0000

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd geral
+M3U_FILE="geral.m3u"
 
 # PT TVI
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
 
 # PT CNN
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
-
-M3U_FILE="geral.m3u"
 
 # FR
 SOURCE_M6="https://www.stream4free.tv/m6-live-streaming"
