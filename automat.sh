1e4f524e30b0534eb00f6f22172c0adc4dcf42a8
mckfq <m.freirequeiros@gmail.com>
2025-03-12 00:42:05 +0000

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -8,6 +8,14 @@ sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/play
 # PT CNN
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
 
-# FR M6
-sed -i "/m6france.m3u8/ c https://sv1.data-stream.top/$(wget -qO- "https://www.stream4free.tv/m6-live-streaming" | grep -oP 'https://sv1\.data-stream\.top/[^"]+/hls/m6france\.m3u8' | head -n 1)/hls/m6france.m3u8/" geral.m3u
+# Étape 1 : Extraire l'URL M3U pour M6
+M6_URL=$(curl -s https://www.stream4free.tv/m6-live-streaming | grep -oP 'https://.*?\.m3u8' | head -n 1)
+
+if [ -n "$M6_URL" ]; then
+    # Mettre à jour ou ajouter l'entrée M6 dans le fichier M3U
+    sed -i "/m6france/ c $M6_URL" "$M3U_FILE"
+    echo "M6 URL mis à jour : $M6_URL"
+else
+    echo "Erreur : Impossible de récupérer l'URL M6."
+
 
