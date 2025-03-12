35c0ebe67720a72ba9060c038d4c58f79cd32068
mckfq <m.freirequeiros@gmail.com>
2025-03-12 00:34:47 +0000

    Update automat.sh

--- automat.sh
+++ automat.sh
@@ -9,5 +9,5 @@ sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/play
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
 
 # FR M6
-#sed -i "/m6france.m3u8/ c https://sv1.data-stream.top/$(wget -qO- "https://www.stream4free.tv/m6-live-streaming" | grep -oP 'https://sv1\.data-stream\.top/[^"]+/hls/m6france\.m3u8' | head -n 1)/hls/m6france.m3u8/" geral.m3u
+sed -i "/m6france.m3u8/ c https://sv1.data-stream.top/$(wget -qO- "https://www.stream4free.tv/m6-live-streaming" | grep -oP 'https://sv1\.data-stream\.top/[^"]+/hls/m6france\.m3u8' | head -n 1)/hls/m6france.m3u8/" geral.m3u
 
