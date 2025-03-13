b53d6986160b9c1edb0fc8972cf7ec0723012ffb
mckfq <m.freirequeiros@gmail.com>
2025-03-13 11:05:27 +0000

    Update automatpt.sh

---
+++ automatpt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+M3U_FILE="geral.m3u"
+
+# PT TVI
+ed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
+
+# PT CNN
+sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" geral.m3u
